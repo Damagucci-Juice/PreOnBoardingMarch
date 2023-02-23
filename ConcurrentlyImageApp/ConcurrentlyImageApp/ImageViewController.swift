c7cd88feb5d9ad86127093aa9119542e8d83b970
Damagucci-Juice <jsr9874@gmail.com>
2023-02-23 10:19:16 +0900

    feat: 프로젝트 생성 및 .gitignore 설정

---
+++ ConcurrentlyImageApp/ConcurrentlyImageApp/ImageViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ImageViewController.swift
+//  ConcurrentlyImageApp
+//
+//  Created by YEONGJIN JANG on 2023/02/23.
+//
+
+import UIKit
+
+//image url https://picsum.photos/200 : 랜덤 이미지
+
+class ImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
