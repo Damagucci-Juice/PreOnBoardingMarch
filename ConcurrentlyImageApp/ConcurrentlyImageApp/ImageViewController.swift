8667fa165cbb0beaecdfb02bca95994838948d3a
Damagucci-Juice <jsr9874@gmail.com>
2023-02-24 13:04:19 +0900

    feat: ImageView와 버튼 기능 구현
    각각에 맞는 코드를 구현하고, 이미지도 할당
    #1

--- ConcurrentlyImageApp/ConcurrentlyImageApp/ImageViewController.swift
+++ ConcurrentlyImageApp/ConcurrentlyImageApp/ImageViewController.swift
@@ -7,15 +7,99 @@
 
 import UIKit
 
-//image url https://picsum.photos/200 : 랜덤 이미지
-
 class ImageViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
     }
-
-
+    
+    @IBOutlet weak var imageView1: UIImageView!
+    
+    @IBOutlet weak var imageView2: UIImageView!
+    
+    @IBOutlet weak var imageView3: UIImageView!
+    
+    @IBOutlet weak var imageView4: UIImageView!
+    
+    @IBOutlet weak var imageView5: UIImageView!
+    
+    
+    @IBAction func tappedLoadImage1(_ sender: Any) {
+        loadImage { image in
+            Task {
+                self.imageView1.image = image
+            }
+        }
+    }
+    
+    @IBAction func tappedLoadImage2(_ sender: Any) {
+        loadImage { image in
+            Task {
+                self.imageView2.image = image
+            }
+        }
+    }
+    
+    @IBAction func tappedLoadImage3(_ sender: Any) {
+        loadImage { image in
+            Task {
+                self.imageView3.image = image
+            }
+        }
+    }
+    
+    @IBAction func tappedLoadImage4(_ sender: Any) {
+        loadImage { image in
+            Task {
+                self.imageView4.image = image
+            }
+        }
+    }
+    
+    @IBAction func tappedLoadImage5(_ sender: Any) {
+        loadImage { image in
+            Task {
+                self.imageView5.image = image
+            }
+        }
+    }
+    
+    @IBAction func tappedLoadAllImages(_ sender: Any) {
+        tappedLoadImage1(sender)
+        tappedLoadImage2(sender)
+        tappedLoadImage3(sender)
+        tappedLoadImage4(sender)
+        tappedLoadImage5(sender)
+    }
+    
+    @IBAction func tappedRemoveAllImages(_ sender: Any) {
+        Task {
+            self.imageView1.image = UIImage(systemName: "photo")
+            self.imageView2.image = UIImage(systemName: "photo")
+            self.imageView3.image = UIImage(systemName: "photo")
+            self.imageView4.image = UIImage(systemName: "photo")
+            self.imageView5.image = UIImage(systemName: "photo")
+        }
+    }
+    
+    private func loadImage(completion: @escaping (UIImage?) -> Void) {
+        guard let url = URL(string: "https://picsum.photos/200")
+        else { return }
+        let urlRequest = URLRequest(url: url)
+        
+        let task = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+            guard error == nil,
+                  let statusCode = (response as? HTTPURLResponse)?.statusCode,
+                  let data = data
+            else { return }
+            if 200..<300 ~= statusCode {
+                return completion(UIImage(data: data))
+            }
+            return completion(nil)
+        }
+        
+        task.resume()
+    }
 }
 
